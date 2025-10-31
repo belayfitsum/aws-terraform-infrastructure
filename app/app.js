f3cb4e1ac5ae2b1a4b6d1c0bafddf481c047277c
fitsum <belay.fitsum@gmail.com>
2025-10-31 22:15:03 +0200

    Update app only

--- app/app.js
+++ app/app.js
@@ -131,7 +131,7 @@ app.delete('/logs', (req, res) => {
   );
 });
 
-// Start the server
+// Start the server - comment
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
